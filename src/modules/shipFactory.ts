bbf101c28acd55554389bc08d3bc6d82bf55f33a
Konsta Nenonen <konstanenonen@protonmail.com>
2022-06-25 16:46:51 +0300

    First version of shipFactory ready, refactoring needed

--- src/modules/shipFactory.ts
+++ src/modules/shipFactory.ts
@@ -1,5 +1,21 @@
-const shipFactory = (length: number) => ({
-  length,
-});
+const shipFactory = (length: number) => {
+  const hitPositions: boolean[] = [];
+  for (let i = 0; i < length; i += 1) {
+    hitPositions.push(false);
+  }
+
+  const hit = (position: number) => {
+    hitPositions[position] = true;
+  };
+
+  const isSunk = () => !hitPositions.includes(false);
+
+  return {
+    length,
+    hitPositions,
+    hit,
+    isSunk,
+  };
+};
 
 export default shipFactory;
--- tests/shipFactory.test.ts
+++ tests/shipFactory.test.ts
@@ -5,3 +5,36 @@ test("shipFactory - returns a ship with correct length", () => {
   const ship = shipFactory(3);
   expect(ship.length).toBe(3);
 });
+
+test("shipFactory - ship should not be sunk with no hits", () => {
+  const ship = shipFactory(3);
+  expect(ship.isSunk()).toBe(false);
+});
+
+test("shipFactory - should not be sunk after some hits", () => {
+  const ship = shipFactory(3);
+  ship.hit(0);
+  ship.hit(2);
+  expect(ship.isSunk()).toBe(false);
+});
+
+test("shipFactory - should be sunk after full hits", () => {
+  const ship = shipFactory(3);
+  ship.hit(0);
+  ship.hit(1);
+  ship.hit(2);
+  expect(ship.isSunk()).toBe(true);
+});
+
+test("shipFactory - should provide correct positions for hits", () => {
+  const smallShip = shipFactory(2);
+  const bigShip = shipFactory(5);
+
+  smallShip.hit(0);
+
+  bigShip.hit(1);
+  bigShip.hit(3);
+
+  expect(smallShip.hitPositions).toEqual([true, false]);
+  expect(bigShip.hitPositions).toEqual([false, true, false, true, false]);
+});
