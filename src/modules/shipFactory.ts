1af8fa53071cce1efd23eb3daa08053230f68f72
Konsta Nenonen <konstanenonen@protonmail.com>
2022-06-26 09:09:58 +0300

    Refactored shipFactory function

--- src/modules/shipFactory.ts
+++ src/modules/shipFactory.ts
@@ -1,13 +1,5 @@
-const createHitPositions = (length: number) => {
-  const positions: boolean[] = [];
-  for (let i = 0; i < length; i += 1) {
-    positions.push(false);
-  }
-  return positions;
-};
-
 const shipFactory = (length: number) => {
-  const hitPositions = createHitPositions(length);
+  const hitPositions = Array.from(Array(length)).map(() => false);
 
   const hit = (position: number) => {
     hitPositions[position] = true;
