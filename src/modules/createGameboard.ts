6a3dfbeca159eaab38641eb5746e42492e65590f
Konsta Nenonen <konstanenonen@protonmail.com>
2022-06-26 11:58:29 +0300

    Done with the first version of gameboard, refactoring needed

--- src/modules/createGameboard.ts
+++ src/modules/createGameboard.ts
@@ -1,5 +1,8 @@
+import createShip from "./createShip";
+
 const createGameboard = () => {
-  const state = Array.from(Array(9)).map(() => [
+  const state = Array.from(Array(10)).map(() => [
+    null,
     null,
     null,
     null,
@@ -11,7 +14,78 @@ const createGameboard = () => {
     null,
   ]);
 
-  return { state };
+  const carrier = createShip(5);
+  const battleship = createShip(4);
+  const cruiser = createShip(3);
+  const submarine = createShip(3);
+  const destroyer = createShip(2);
+
+  state[9][5] = ["carrier", 0];
+  state[9][6] = ["carrier", 1];
+  state[9][7] = ["carrier", 2];
+  state[9][8] = ["carrier", 3];
+  state[9][9] = ["carrier", 4];
+
+  state[0][0] = ["battleship", 0];
+  state[0][1] = ["battleship", 1];
+  state[0][2] = ["battleship", 2];
+  state[0][3] = ["battleship", 3];
+
+  state[7][4] = ["submarine", 0];
+  state[7][5] = ["submarine", 1];
+  state[7][6] = ["submarine", 2];
+
+  state[3][9] = ["cruiser", 0];
+  state[4][9] = ["cruiser", 1];
+  state[5][9] = ["cruiser", 2];
+
+  state[1][5] = ["destroyer", 0];
+  state[1][6] = ["destroyer", 1];
+
+  const receiveAttack = (y: number, x: number) => {
+    const boardTile = state[y][x];
+    if (boardTile === "miss" || boardTile === "hit") {
+      return;
+    }
+    if (boardTile === null) {
+      state[y][x] = "miss";
+      return;
+    }
+
+    const shipName = boardTile[0];
+    const hitPosition = boardTile[1];
+
+    switch (shipName) {
+      case "carrier":
+        carrier.hit(hitPosition);
+        break;
+      case "battleship":
+        battleship.hit(hitPosition);
+        break;
+      case "cruiser":
+        cruiser.hit(hitPosition);
+        break;
+      case "submarine":
+        submarine.hit(hitPosition);
+        break;
+      case "destroyer":
+        destroyer.hit(hitPosition);
+        break;
+      default:
+        break;
+    }
+
+    state[y][x] = "hit";
+  };
+
+  const allShipsSunk = () =>
+    destroyer.isSunk() &&
+    battleship.isSunk() &&
+    cruiser.isSunk() &&
+    submarine.isSunk() &&
+    carrier.isSunk();
+
+  return { state, receiveAttack, allShipsSunk };
 };
 
 export default createGameboard;
--- tests/createGameboard.test.ts
+++ tests/createGameboard.test.ts
@@ -1,17 +1,122 @@
 /* global test expect */
 import createGameboard from "../src/modules/createGameboard";
 
-test("createGameboard - should have 9x9 two dimensonal array", () => {
+test("createGameboard - should have 10x10 two dimensonal array", () => {
+  const gameboard = createGameboard();
+  expect(gameboard.state.length).toBe(10);
+  for (let i = 0; i < gameboard.state.length; i += 1) {
+    expect(gameboard.state[i].length).toBe(10);
+  }
+});
+
+test("createGameboard - should place ships to gameboard", () => {
   const gameboard = createGameboard();
   expect(gameboard.state).toEqual([
-    [null, null, null, null, null, null, null, null, null],
-    [null, null, null, null, null, null, null, null, null],
-    [null, null, null, null, null, null, null, null, null],
-    [null, null, null, null, null, null, null, null, null],
-    [null, null, null, null, null, null, null, null, null],
-    [null, null, null, null, null, null, null, null, null],
-    [null, null, null, null, null, null, null, null, null],
-    [null, null, null, null, null, null, null, null, null],
-    [null, null, null, null, null, null, null, null, null],
+    [
+      ["battleship", 0],
+      ["battleship", 1],
+      ["battleship", 2],
+      ["battleship", 3],
+      null,
+      null,
+      null,
+      null,
+      null,
+      null,
+    ],
+    [
+      null,
+      null,
+      null,
+      null,
+      null,
+      ["destroyer", 0],
+      ["destroyer", 1],
+      null,
+      null,
+      null,
+    ],
+    [null, null, null, null, null, null, null, null, null, null],
+    [null, null, null, null, null, null, null, null, null, ["cruiser", 0]],
+    [null, null, null, null, null, null, null, null, null, ["cruiser", 1]],
+    [null, null, null, null, null, null, null, null, null, ["cruiser", 2]],
+    [null, null, null, null, null, null, null, null, null, null],
+    [
+      null,
+      null,
+      null,
+      null,
+      ["submarine", 0],
+      ["submarine", 1],
+      ["submarine", 2],
+      null,
+      null,
+      null,
+    ],
+    [null, null, null, null, null, null, null, null, null, null],
+    [
+      null,
+      null,
+      null,
+      null,
+      null,
+      ["carrier", 0],
+      ["carrier", 1],
+      ["carrier", 2],
+      ["carrier", 3],
+      ["carrier", 4],
+    ],
   ]);
+});
+
+test("createGameboard - receiveAttack function takes coordinates and marks the spot missed", () => {
+  const gameboard = createGameboard();
+  gameboard.receiveAttack(1, 0);
+  expect(gameboard.state[1][0]).toBe("miss");
+});
+
+test("createGameboard - receiveAttack function marks the sopt hit when there is a ship", () => {
+  const gameboard = createGameboard();
+  gameboard.receiveAttack(0, 0);
+  expect(gameboard.state[0][0]).toBe("hit");
+});
+
+test("createGameboard - allShipsSunk should return false if all ships haven't sunk", () => {
+  const gameboard = createGameboard();
+  gameboard.receiveAttack(0, 0);
+  gameboard.receiveAttack(0, 1);
+  gameboard.receiveAttack(0, 2);
+  gameboard.receiveAttack(0, 3);
+
+  gameboard.receiveAttack(1, 5);
+  gameboard.receiveAttack(1, 6);
+
+  expect(gameboard.allShipsSunk()).toBe(false);
+});
+
+test("createGameboard - allShipsSunk should return true when all ships are sunk", () => {
+  const gameboard = createGameboard();
+  gameboard.receiveAttack(3, 9);
+  gameboard.receiveAttack(4, 9);
+  gameboard.receiveAttack(5, 9);
+
+  gameboard.receiveAttack(9, 5);
+  gameboard.receiveAttack(9, 6);
+  gameboard.receiveAttack(9, 7);
+  gameboard.receiveAttack(9, 8);
+  gameboard.receiveAttack(9, 9);
+
+  gameboard.receiveAttack(0, 0);
+  gameboard.receiveAttack(0, 1);
+  gameboard.receiveAttack(0, 2);
+  gameboard.receiveAttack(0, 3);
+
+  gameboard.receiveAttack(7, 4);
+  gameboard.receiveAttack(7, 5);
+  gameboard.receiveAttack(7, 6);
+
+  gameboard.receiveAttack(1, 5);
+  gameboard.receiveAttack(1, 6);
+
+  expect(gameboard.allShipsSunk()).toBe(true);
 });
